67aba7b88877e52e171831376bc6ff85876d6a8d
Ishita Kunjekar <ishitakunjekar123@gmail.com>
2026-06-22 18:39:40 +0530

    Day 1 - Variables and Data Types

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 
 class Hello
 {
-    public static void main (String args[])
-
-    {
-        int num1 = 3;
+    public static void main (String args[]) {
+        int num1 = 25;
         byte num2 = 127;
         double num3 = 8.5;
         char c = 'k';
-
+        long num4 = 5678l;
+        num2 = (byte) num1;
+        System.out.println(num2);
     }
 }
