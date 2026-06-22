97d742424070d7080c6d8f29cba6a532eb6251a0
Ishita Kunjekar <ishitakunjekar123@gmail.com>
2026-06-22 18:14:07 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+class Hello
+{
+    public static void main (String args[])
+
+    {
+        int num1 = 3;
+        byte num2 = 127;
+        double num3 = 8.5;
+        char c = 'k';
+
+    }
+}
